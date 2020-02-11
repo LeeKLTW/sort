212dd26279849c9b80323e048c393ba03c0d49b3
LeeKLTW <LeeKLTW@gmail.com>
2020-02-11 18:44:15 +0800

    add print function

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -26,14 +26,25 @@ void bubbleSort(int A[], int n){
     }
 }
 
-
-int main(){
-    int A[] = {11,13,7,12,16,9,24,5,10,3}, n =10, i;
-    bubbleSort(A, n);
-    for(int i=0; i<n; i++){
-        cout<<A[i]<<",";
+void print(int A[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << ",";
     }
     cout << endl;
+}
+
+
+int main() {
+    int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3}, n = 10;
+
+    cout << "Before sort" << endl;
+    print(A, n);
+
+    bubbleSort(A, n);
+
+    cout << "After sort" << endl;
+    print(A, n);
+
     return 0;
 }
 
