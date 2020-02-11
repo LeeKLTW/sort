4c657fe6dc7e59ce7bb15ed71055201822be4f38
LeeKLTW <LeeKLTW@gmail.com>
2020-02-11 14:15:10 +0800

    bubble sort main function part

---
+++ bubbleSort.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+
+
+int main(){
+    int A[] = {11,13,7,12,16,9,24,5,10,3}, n =10, i;
+    bubbleSort(A, n);
+    for(int i=0; i<n; i++){
+        cout<<A[i];
+    }
+    cout << endl;
+    return 0;
+}
+
