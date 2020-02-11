ecbaf3dc331ccc46c9e5f761188ef9e887c4f8e6
LeeKLTW <LeeKLTW@gmail.com>
2020-02-11 14:21:29 +0800

    add bubble sort function

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -5,6 +5,23 @@ using namespace std;
 
 
 
+void bubbleSort(int A[], int n){
+    int i,j,flag=0;
+    for(i=0; i<n-1; i++){
+        flag = 0;
+        for (j=0; j<n-i-1;j++){
+            if (A[j]>A[j+1]){
+                swap(&A[j], &A[j+1]);
+                flag =1;
+            }
+        }
+        if (flag==0){
+            break;
+        }
+    }
+}
+
+
 int main(){
     int A[] = {11,13,7,12,16,9,24,5,10,3}, n =10, i;
     bubbleSort(A, n);
