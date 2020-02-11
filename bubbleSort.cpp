596e326567b89c16aa69618aa791b38a3d5116bb
LeeKLTW <LeeKLTW@gmail.com>
2020-02-11 14:21:07 +0800

    add iostream

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 using namespace std;
 
 
@@ -8,7 +9,7 @@ int main(){
     int A[] = {11,13,7,12,16,9,24,5,10,3}, n =10, i;
     bubbleSort(A, n);
     for(int i=0; i<n; i++){
-        cout<<A[i];
+        cout<<A[i]<<",";
     }
     cout << endl;
     return 0;
