87b06a1482938b33661b02a1852e00befee8af05
LeeKLTW <LeeKLTW@gmail.com>
2020-02-11 18:44:50 +0800

    remove redundant declaration & wording

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+
 using namespace std;
 
-void swap(int *x, int *y){
-    int temp =*x;
+void swap(int *x, int *y) {
+    int temp = *x;
     *x = *y;
     *y = temp;
 }
 
 
-void bubbleSort(int A[], int n){
-    int i,j,flag=0;
-    for(i=0; i<n-1; i++){
+void bubbleSort(int A[], int n) {
+    int flag = 0;
+    for (int i = 0; i < n - 1; i++) {
         flag = 0;
-        for (j=0; j<n-i-1;j++){
-            if (A[j]>A[j+1]){
-                swap(&A[j], &A[j+1]);
-                flag =1;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (A[j] > A[j + 1]) {
+                swap(&A[j], &A[j + 1]);
+                flag = 1;
             }
         }
-        if (flag==0){
+        if (flag == 0) {
             break;
         }
     }
