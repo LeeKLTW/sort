d173b5041b2cffca0c6da845036d0771ebb1cfbc
LeeKLTW <LeeKLTW@gmail.com>
2020-02-12 20:46:23 +0800

    complete selectionSort.cpp

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -3,6 +3,24 @@
 #include <iostream>
 
 using namespace std;
+void swap(int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+
+void selectionSort(int A[], int n){
+    int i, j, k;
+    for (int i=0; i<n-1;i++){
+        for (int j=k=i; j<n; j++){
+            if (A[j]<A[k]){
+                k=j;
+            }
+            swap(&A[i], &A[k]);
+        }
+    }
+}
 
 
 void print(int A[], int n) {
@@ -19,7 +37,7 @@ int main() {
     cout << "Before sort" << endl;
     print(A, n);
 
-//    selectionSort(A, n);
+    selectionSort(A, n);
 
     cout << "After sort" << endl;
     print(A, n);
