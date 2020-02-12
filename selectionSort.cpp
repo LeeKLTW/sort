0600168df109b777c4bac2c8017e6b97256ba809
LeeKLTW <LeeKLTW@gmail.com>
2020-02-12 20:41:54 +0800

    init selectionSort with main function

---
+++ selectionSort.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+
+void print(int A[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << ",";
+    }
+    cout << endl;
+}
+
+
+int main() {
+    int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3}, n = 10;
+
+    cout << "Before sort" << endl;
+    print(A, n);
+
+//    selectionSort(A, n);
+
+    cout << "After sort" << endl;
+    print(A, n);
+
+    return 0;
+}
+
